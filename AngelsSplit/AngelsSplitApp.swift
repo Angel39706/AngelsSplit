736cb88f1f793aa2b85e5e3e9564b9c1063536fc
Angel39706 <96604953+Angel39706@users.noreply.github.com>
2022-12-19 15:31:02 -0800

    Initial Commit

---
+++ AngelsSplit/AngelsSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngelsSplitApp.swift
+//  AngelsSplit
+//
+//  Created by Angel Martinez on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AngelsSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
